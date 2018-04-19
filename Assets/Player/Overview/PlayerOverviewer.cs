4e5f20e176425a2569e553fc4bf80697a474e332
ConnorWorrell <chemicalcoal1@gmail.com>
2018-04-18 19:31:07 -0600

    Made Blue Circles Spawnable by player, circles also destroy after animation has played

---
+++ Assets/Player/Overview/PlayerOverviewer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOverviewer : MonoBehaviour {
+
+	//Variables that are to be read by other things
+	public bool MoveForward = false, MoveBackward = false, MoveLeft = false, MoveRight = false;
+	public bool LeftClick = false, RightClick = false, CenterClick = false;
+	public float Mousex = 0, Mousey = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Movement Intentions
+		MoveForward = Input.GetKey (KeyCode.W);
+		MoveBackward = Input.GetKey (KeyCode.S);
+		MoveLeft = Input.GetKey (KeyCode.A);
+		MoveRight = Input.GetKey (KeyCode.D);
+
+		//Mouse Intentions
+		LeftClick = Input.GetMouseButton (0);
+		RightClick = Input.GetMouseButton (1);
+		CenterClick = Input.GetMouseButton (2);
+		Mousex = Input.mousePosition.x;
+		Mousey = Input.mousePosition.y;
+
+
+	}
+}
---
+++ Assets/Player/Player1/Player1.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+	//Game Objects that are needed for the player to function
+	public GameObject PlayerOverviewerPrefab, SpawnPoint, BlueCircle;
+	private GameObject Circle;
+
+	//PowerSupplied is ammount of power put into circle, power supplied multiplier changes rate of power supply
+	public float PowerSupplied = 0, PowerSuppliedMultiplier = 20;
+
+	//LeftClick for to add power, right click to subtract power
+	public bool Shooting = false, UnShooting = false;
+
+	// Use this for initialization
+	void Start () {
+		//Add PlayerOverviewer to the Scene if it is not alreay in the scene
+		if (GameObject.Find ("PlayerOverviewer") == null) {
+			GameObject go = Instantiate (PlayerOverviewerPrefab, new Vector3 (0, 0, 0), Quaternion.identity);
+			go.name = go.name.Replace ("(Clone)", "");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//GetShooting and Unshooting from player overviewer
+		Shooting = GameObject.Find ("PlayerOverviewer").GetComponent<PlayerOverviewer> ().LeftClick;
+		UnShooting = GameObject.Find ("PlayerOverviewer").GetComponent<PlayerOverviewer> ().RightClick;
+
+		//If there is not a circle present no power is supplied
+		if(GameObject.Find("Blue1(Clone)") == null)
+			PowerSupplied = 0;
+
+		// If power is being removed from the circle, and circle has no power and not holding circle power, remove circle
+		if (UnShooting && PowerSupplied == 0 && !Shooting)
+			Destroy (Circle);
+
+		//If adding or removing power from circle
+		if (Shooting || UnShooting) {
+			//if no circle make a circle
+			if (GameObject.Find ("Blue1(Clone)") == null && Shooting)
+				Circle = Instantiate (BlueCircle, SpawnPoint.transform);
+			//Calculate power being supplied to the circle
+			PowerSupplied = PowerSupplied + PowerSuppliedMultiplier * Time.deltaTime * ((Shooting ? 1 : 0) - (UnShooting ? 1 : 0));
+			PowerSupplied = PowerSupplied < 0 ? 0 : PowerSupplied > 40 ? 40 : PowerSupplied;
+			//Send power to circle if there is a circle
+			if(GameObject.Find ("Blue1(Clone)") != null)
+				Circle.GetComponent<BlueCircleController> ().Power = PowerSupplied;
+		} else {
+			//If Not fuleing or unfueling, and circle has power, shoot circle
+			if (GameObject.Find ("Blue1(Clone)") != null && PowerSupplied > 0)
+				Circle.GetComponent<BlueCircleController> ().Shoot = true;
+			//If not fueling or unfueling and circle has no pwer, remove circle
+			else if (PowerSupplied == 0)
+				Destroy (Circle);
+		}
+
+	}
+}
