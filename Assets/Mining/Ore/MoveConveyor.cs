a4ec2f0276e67822700afadf7f69a5bddd7cc730
ConnorWorrell <chemicalcoal1@gmail.com>
2018-04-24 19:20:05 -0600

    Ore now spread evenly over conveyor, Prefabed: Dropper, CircularOre, Conveyor

--- Assets/Mining/Ore/MoveConveyor.cs
+++ Assets/Mining/Ore/MoveConveyor.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class MoveConveyor : MonoBehaviour {
 
-	public float PushDistance = 0.25f, Drag = 0.25f, RotationalDragMult = 1, InitialDrag = 0;
+	public float RangeLow = 0f, RangeHigh = 1f;
+
+	public float PushDistance = 0.25f, Drag = 0.25f, RotationalDragMult = 1, InitialDrag = 0, ForceLagTime = 0;
 	public bool KillOnImpact = true;
 
-	public Vector3 ForceDirection;
+	public Vector3 ForceDirection, ForceLast;
 
 	private Rigidbody rB;
 
 
 	// Use this for initialization
 	void Start () {
+
+		ForceLast = new Vector3 (0, 0, 0);
+
 		//Record RidigdBody of object and the inital drag, ititial drag will be used for 
 		//the player when we don't want to apply drag when not interacting with the conveyor belts
 		rB = gameObject.GetComponent<Rigidbody> ();
 		InitialDrag = Drag;
+
+		if (ForceLagTime == 0)
+			ForceLagTime = Random.Range (RangeLow, RangeHigh);
 	}
 	
 	// Update is called once per frame
@@ -29,8 +37,10 @@ public class MoveConveyor : MonoBehaviour {
 
 		//If not sitting on something apply no force, turn off drag for player
 		if (hit.collider == null) {
-			if(KillOnImpact == false)
+			ForceLast = new Vector3 (0, 0, 0);
+			if (KillOnImpact == false) {
 				Drag = 0;
+			}
 			ForceDirection = new Vector3 (0, 0, 0);
 
 		//If the object is sitting on conveyor belt apply drag and get force direction of conveyor
@@ -41,8 +51,10 @@ public class MoveConveyor : MonoBehaviour {
 
 		//If object is sitting on something that isn't a conveyor, kill if not player and apply no force
 		} else {
-			if(KillOnImpact == false)
+			if (KillOnImpact == false) {
 				Drag = 0;
+				ForceLast = new Vector3 (0, 0, 0);
+			}
 			ForceDirection = new Vector3 (0, 0, 0);
 			if(KillOnImpact == true && (hit.collider.transform.parent == null || hit.collider.transform.parent.gameObject.name.Substring (0, 3) != "Ore"))
 				Destroy (gameObject);
@@ -52,8 +64,12 @@ public class MoveConveyor : MonoBehaviour {
 		Vector3 Velocity = rB.velocity;
 		ForceDirection.Set (ForceDirection.x - Velocity.x * Drag, ForceDirection.y, ForceDirection.z - Velocity.z * Drag);
 
+		ForceDirection = (ForceDirection + ForceLast * ForceLagTime * Time.fixedTime) / (1 + ForceLagTime * Time.fixedTime);
+
 		//add force at the point of contact between the object and conveyor
 		rB.AddForceAtPosition (ForceDirection*Time.fixedTime, hit.point);
+
+		ForceLast = ForceDirection;
 
 
 
